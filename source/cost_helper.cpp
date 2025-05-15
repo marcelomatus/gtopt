bdf0adff4669c931db9dbe99a41ce868d01bf168
Marcelo Matus <mmatus@blackbeareng.cl>
2025-05-15 18:29:36 -0400

    refactor: simplify cost factor calculations using ranges

--- source/cost_helper.cpp
+++ source/cost_helper.cpp
@@ -1,22 +1,26 @@
+#include <utility>
+
 #include <gtopt/cost_helper.hpp>
 #include <gtopt/utils.hpp>
-
+#include <range/v3/all.hpp>
 namespace gtopt
 {
 
 auto CostHelper::block_icost_factors() const -> block_factor_matrix_t
 {
-  const auto n_scenarios = static_cast<Index>(m_scenarios_.get().size());
-  const auto n_stages = static_cast<Index>(m_stages_.get().size());
+  auto&& active_scenarios = enumerate_active<Index>(m_scenarios_.get());
+  auto&& active_stages = enumerate_active<Index>(m_stages_.get());
+
+  const auto n_scenarios = ranges::distance(active_scenarios);
+  const auto n_stages = ranges::distance(active_stages);
   block_factor_matrix_t factors(boost::extents[n_scenarios][n_stages]);
 
-  for (auto&& [si, scenario] : enumerate_active<Index>(m_scenarios_.get())) {
-    for (auto&& [ti, stage] : enumerate_active<Index>(m_stages_.get())) {
-      factors[si][ti].resize(stage.blocks().size());
-      for (auto&& [bi, block] : enumerate<Index>(stage.blocks())) {
-        const auto cfactor = cost_factor(scenario, stage, block);
-        factors[si][ti][bi] = 1.0 / cfactor;
-      }
+  for (auto&& [si, scenario] : active_scenarios) {
+    for (auto&& [ti, stage] : active_stages) {
+      factors[si][ti] =
+          to_vector(stage.blocks(),
+                    [&](const auto& block)
+                    { return 1.0 / cost_factor(scenario, stage, block); });
     }
   }
 
@@ -26,25 +30,25 @@ auto CostHelper::block_icost_factors() const -> block_factor_matrix_t
 auto CostHelper::stage_icost_factors(double probability) const
     -> stage_factor_matrix_t
 {
-  stage_factor_matrix_t factors(m_stages_.get().size());
+  auto&& active_stages = enumerate_active<Index>(m_stages_.get());
 
-  for (auto&& [ti, stage] : enumerate_active<Index>(m_stages_.get())) {
-    const auto cfactor = cost_factor(stage, probability);
-    factors[ti] = 1.0 / cfactor;
-  }
-
-  return factors;
+  return to_vector(active_stages,
+                   [&](const auto& is)
+                   { return 1.0 / cost_factor(is.second, probability); });
 }
 
 auto CostHelper::scenario_stage_icost_factors() const
     -> scenario_stage_factor_matrix_t
 {
-  const auto n_scenarios = static_cast<Index>(m_scenarios_.get().size());
-  const auto n_stages = static_cast<Index>(m_stages_.get().size());
+  auto&& active_scenarios = enumerate_active<Index>(m_scenarios_.get());
+  auto&& active_stages = enumerate_active<Index>(m_stages_.get());
+
+  const auto n_scenarios = ranges::distance(active_scenarios);
+  const auto n_stages = ranges::distance(active_stages);
   scenario_stage_factor_matrix_t factors(boost::extents[n_scenarios][n_stages]);
 
-  for (auto&& [si, scenario] : enumerate_active<Index>(m_scenarios_.get())) {
-    for (auto&& [ti, stage] : enumerate_active<Index>(m_stages_.get())) {
+  for (auto&& [si, scenario] : active_scenarios) {
+    for (auto&& [ti, stage] : active_stages) {
       const auto cfactor = cost_factor(scenario, stage);
       factors[si][ti] = 1.0 / cfactor;
     }
