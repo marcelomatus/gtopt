94e2e41b411bb783af02a686cdd6c2f6504f91bf
Marcelo Matus <mmatus@blackbeareng.cl>
2025-06-11 15:07:45 -0400

    refactor: simplify InputTraits by removing unused type aliases

--- include/gtopt/input_traits.hpp
+++ include/gtopt/input_traits.hpp
@@ -10,67 +10,21 @@
 
 #pragma once
 
-#include <gtopt/uid_traits.hpp>
 #include <gtopt/mvector_traits.hpp>
+#include <gtopt/uid_traits.hpp>
 #include <gtopt/utils.hpp>
 #include <spdlog/spdlog.h>
 
-namespace gtopt {
+namespace gtopt
+{
 
 /**
  * @brief Input data access traits
- * 
+ *
  * Inherits from UidTraits and adds input-specific functionality
  */
-struct InputTraits : UidTraits {
-  template<typename Key, typename Value>
-  using base_map_t = gtopt::flat_map<Key, Value>;
-
-  using ClassNameType = std::string_view;
-  using FieldNameType = std::string_view;
-  using CFNameUid = std::tuple<ClassNameType, FieldNameType, Uid>;
-  using CFName = std::tuple<ClassNameType, FieldNameType>;
-
-  using ArrowChunkedArray = gtopt::ArrowChunkedArray;
-  using ArrowTable = gtopt::ArrowTable;
-
-  template<typename... Uid>
-  using ArrowUidIdx = typename UidToArrowIdx<Uid...>::UidIdx;
-
-  template<typename... Uid>
-  using arrow_array_uid_idx_t =
-      std::tuple<ArrowChunkedArray, ArrowUidIdx<Uid...>>;
-
-  template<typename... Uid>
-  using array_uid_idx_map_t =
-      base_map_t<CFNameUid, arrow_array_uid_idx_t<Uid...>>;
-
-  template<typename... Uid>
-  using arrow_table_uid_idx_t = std::tuple<ArrowTable, ArrowUidIdx<Uid...>>;
-
-  template<typename... Uid>
-  using arrow_table_map_t = base_map_t<CFName, arrow_table_uid_idx_t<Uid...>>;
-
-  template<typename... Uid>
-  using table_uid_idx_map_t = base_map_t<CFName, arrow_table_uid_idx_t<Uid...>>;
-
-  template<typename... Uid>
-  using VectorUidIdx = typename UidToVectorIdx<Uid...>::UidIdx;
-
-  template<typename... Uid>
-  using vector_uid_idx_t = VectorUidIdx<Uid...>;
-
-  template<typename... Uid>
-  using idx_key_t = typename UidToVectorIdx<Uid...>::IndexKey;
-
-  template<typename value_type, typename... Uid>
-  using idx_vector_t =
-      typename mvector_traits<value_type, idx_key_t<Uid...>>::vector_type;
-
-  template<typename... Uid>
-  using array_table_vector_uid_idx_t = std::tuple<array_uid_idx_map_t<Uid...>,
-                                                  table_uid_idx_map_t<Uid...>,
-                                                  vector_uid_idx_t<Uid...>>;
+struct InputTraits : UidTraits
+{
   template<typename... Uid>
   using array_vector_uid_idx_v =
       std::variant<arrow_array_uid_idx_t<Uid...>, vector_uid_idx_t<Uid...>>;
@@ -88,7 +42,7 @@ struct InputTraits : UidTraits {
   static constexpr auto access_sched(const FSched& sched,
                                      const UidIdx& uid_idx,
                                      AccessOper access_oper,
-                                     Uid... uid)
+                                     Uid... uid) -> RType
   {
     using value_type = Type;
     using vector_uid_idx_t = vector_uid_idx_t<Uid...>;
@@ -108,10 +62,10 @@ struct InputTraits : UidTraits {
       const auto& [array, a_uid_idx] = std::get<a_uid_idx_type>(uid_idx);
       if (array && a_uid_idx) {
         using array_value = typename arrow::CTypeTraits<Type>::ArrayType;
-        return access_oper(
-            std::static_pointer_cast<array_value>(array->chunk(0)),
-            a_uid_idx,
-            std::make_tuple(uid...));
+        return RType {
+            access_oper(std::static_pointer_cast<array_value>(array->chunk(0)),
+                        a_uid_idx,
+                        std::make_tuple(uid...))};
       }
     }
 
