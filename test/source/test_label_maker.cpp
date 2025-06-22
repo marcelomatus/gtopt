06f1c7dd287c337240d527fab4297a7f13f22b49
Marcelo Matus <mmatus@blackbeareng.cl>
2025-06-22 16:02:59 -0400

    refactor: generalize label generation methods and simplify dependencies

--- test/source/test_label_maker.cpp
+++ test/source/test_label_maker.cpp
@@ -20,50 +20,52 @@ TEST_CASE("basic label generation")
   stages.emplace_back(Stage {.uid = Uid {1}, .name = "stage1"});
   stages.emplace_back(Stage {.uid = Uid {2}, .name = "stage2"});
 
-  const Block block1 {.uid = Uid {1}, .name = "block1"};
-  const Block block2 {.uid = Uid {2}, .name = "block2"};
-  const BlockLP block1_lp {block1};
-  const BlockLP block2_lp {block2};
+  Block block1 {.uid = Uid {1}, .name = "block1"};
+  Block block2 {.uid = Uid {2}, .name = "block2"};
+  BlockLP block1_lp {block1};
+  BlockLP block2_lp {block2};
 
   {
     options.use_lp_names = true;  // Access private member for test
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.label("var") == "var");
-    REQUIRE(maker.label("prefix", "var") == "prefix_var");
-    REQUIRE(maker.label("a", "b", "c") == "a_b_c");
+    REQUIRE(maker.lp_label("var") == "var");
+    REQUIRE(maker.lp_label("prefix", "var") == "prefix_var");
+    REQUIRE(maker.lp_label("a", "b", "c") == "a_b_c");
   }
 
   {
     Options options;
     options.use_lp_names = false;
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.label("var").empty());
-    REQUIRE(maker.label("prefix", "var").empty());
+    REQUIRE(maker.lp_label("var").empty());
+    REQUIRE(maker.lp_label("prefix", "var").empty());
   }
 
   {
     Options options;
     options.use_lp_names = true;
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.t_label(stages[0], "var", "x", "y") == "var_x_y_1");
-    REQUIRE(maker.t_label(stages[1], "a", "b", "c") == "a_b_c_2");
+    auto& st0 = stages[0];
+    StageLP& st1 = stages[1];
+    REQUIRE(maker.lp_label(st0, "var", "x", "y") == "var_x_y_1");
+    REQUIRE(maker.lp_label(st1, "a", "b", "c") == "a_b_c_2");
   }
 
   {
     Options options;
     options.use_lp_names = true;
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.st_label(scenarios[0], stages[0], "var", "x", "y")
+    REQUIRE(maker.lp_label(scenarios[0], stages[0], "var", "x", "y")
             == "var_x_y_1_1");
-    REQUIRE(maker.st_label(scenarios[1], stages[1], "a", "b", "c")
+    REQUIRE(maker.lp_label(scenarios[1], stages[1], "a", "b", "c")
             == "a_b_c_2_2");
   }
 
@@ -71,11 +73,11 @@ TEST_CASE("basic label generation")
     Options options;
     options.use_lp_names = true;
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.stb_label(scenarios[0], stages[0], block1_lp, "var", "a", "b")
+    REQUIRE(maker.lp_label(scenarios[0], stages[0], block1_lp, "var", "a", "b")
             == "var_a_b_1_1_1");
-    REQUIRE(maker.stb_label(scenarios[1], stages[1], block2_lp, "a", "b", "c")
+    REQUIRE(maker.lp_label(scenarios[1], stages[1], block2_lp, "a", "b", "c")
             == "a_b_c_2_2_2");
   }
 
@@ -83,11 +85,11 @@ TEST_CASE("basic label generation")
     Options options;
     options.use_lp_names = true;
     OptionsLP non_const_options {options};
-    LabelMaker maker(non_const_options, scenarios, stages);
+    LabelMaker maker(non_const_options);
 
-    REQUIRE(maker.stb_label(scenarios[0], stages[0], block1_lp, "var", "a", "b")
+    REQUIRE(maker.lp_label(scenarios[0], stages[0], block1_lp, "var", "a", "b")
             == "var_a_b_1_1_1");
-    REQUIRE(maker.stb_label(scenarios[1], stages[1], block2_lp, "a", "b", "c")
+    REQUIRE(maker.lp_label(scenarios[1], stages[1], block2_lp, "a", "b", "c")
             == "a_b_c_2_2_2");
   }
 }
@@ -107,10 +109,10 @@ TEST_CASE("edge cases")
   stages.emplace_back(Stage {.uid = Uid {1}, .name = "stage1"});
   stages.emplace_back(Stage {.uid = Uid {2}, .name = "stage2"});
 
-  LabelMaker maker(options, scenarios, stages);
+  LabelMaker maker(options);
 
   {
-    REQUIRE(maker.label() == "");
-    REQUIRE(maker.t_label(stages[0], "a", "b", "c") == "a_b_c_1");
+    REQUIRE(maker.lp_label() == "");
+    REQUIRE(maker.lp_label(stages[0], "a", "b", "c") == "a_b_c_1");
   }
 }
