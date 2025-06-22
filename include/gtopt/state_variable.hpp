3d051c9db37270d1a77d8b28ff6fb78de93ab4c6
Marcelo Matus <mmatus@blackbeareng.cl>
2025-06-22 13:20:33 -0400

    refactor: use LPKey in add_dependent_variable method

--- include/gtopt/state_variable.hpp
+++ include/gtopt/state_variable.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
+#include <compare>
+#include <span>
+#include <string_view>
+#include <vector>
+
 #include <gtopt/basic_types.hpp>
 #include <gtopt/linear_problem.hpp>
 #include <gtopt/phase.hpp>
 #include <gtopt/scenario.hpp>
 #include <gtopt/scene.hpp>
 #include <gtopt/stage.hpp>
-
-#include <compare>
-#include <span>
-#include <string_view>
-#include <vector>
 
 namespace gtopt
 {
@@ -40,51 +40,55 @@ public:
 
   [[nodiscard]]
   static constexpr auto key(std::string_view class_name,
-                           Uid uid,
-                           std::string_view col_name,
-                           PhaseIndex phase_index,
-                           StageUid stage_uid,
-                           SceneIndex scene_index = SceneIndex{unknown_index},
-                           ScenarioUid scenario_uid = ScenarioUid{unknown_uid}) noexcept -> Key
+                            Uid uid,
+                            std::string_view col_name,
+                            PhaseIndex phase_index,
+                            StageUid stage_uid,
+                            SceneIndex scene_index = SceneIndex {unknown_index},
+                            ScenarioUid scenario_uid = ScenarioUid {
+                                unknown_uid}) noexcept -> Key
   {
-    return {
-      .lp_key = {.scene_index = scene_index, .phase_index = phase_index},
-      .scenario_uid = scenario_uid,
-      .stage_uid = stage_uid,
-      .uid = uid,
-      .class_name = class_name,
-      .col_name = col_name
-    };
+    return {.lp_key = {.scene_index = scene_index, .phase_index = phase_index},
+            .scenario_uid = scenario_uid,
+            .stage_uid = stage_uid,
+            .uid = uid,
+            .class_name = class_name,
+            .col_name = col_name};
   }
 
   template<typename ScenarioLP, typename StageLP>
   [[nodiscard]]
   static constexpr auto key(const ScenarioLP& scenario,
-                           const StageLP& stage,
-                           std::string_view class_name,
-                           Uid element_uid,
-                           std::string_view col_name) noexcept -> Key
+                            const StageLP& stage,
+                            std::string_view class_name,
+                            Uid element_uid,
+                            std::string_view col_name) noexcept -> Key
   {
-    return key(class_name, element_uid, col_name,
-               stage.phase_index(), stage.uid(),
-               scenario.scene_index(), scenario.uid());
+    return key(class_name,
+               element_uid,
+               col_name,
+               stage.phase_index(),
+               stage.uid(),
+               scenario.scene_index(),
+               scenario.uid());
   }
 
   template<typename StageLP>
   [[nodiscard]]
   static constexpr auto key(const StageLP& stage,
-                           std::string_view class_name,
-                           Uid element_uid,
-                           std::string_view col_name) noexcept -> Key
+                            std::string_view class_name,
+                            Uid element_uid,
+                            std::string_view col_name) noexcept -> Key
   {
-    return key(class_name, element_uid, col_name,
-               stage.phase_index(), stage.uid());
+    return key(
+        class_name, element_uid, col_name, stage.phase_index(), stage.uid());
   }
 
-  constexpr explicit StateVariable(LPKey lp_key, Index col) noexcept
-    : m_lp_key_{std::move(lp_key)}
-    , m_col_{col}
-  {}
+  constexpr explicit StateVariable(const LPKey& lp_key, Index col) noexcept
+      : m_lp_key_ {lp_key}
+      , m_col_ {col}
+  {
+  }
 
   struct DependentVariable
   {
@@ -93,43 +97,56 @@ public:
   };
 
   [[nodiscard]]
-  constexpr Index col() const noexcept { return m_col_; }
+  constexpr Index col() const noexcept
+  {
+    return m_col_;
+  }
 
   [[nodiscard]]
-  constexpr const LPKey& lp_key() const noexcept { return m_lp_key_; }
+  constexpr const LPKey& lp_key() const noexcept
+  {
+    return m_lp_key_;
+  }
 
   [[nodiscard]]
-  constexpr SceneIndex scene_index() const noexcept { return m_lp_key_.scene_index; }
+  constexpr SceneIndex scene_index() const noexcept
+  {
+    return m_lp_key_.scene_index;
+  }
 
   [[nodiscard]]
-  constexpr PhaseIndex phase_index() const noexcept { return m_lp_key_.phase_index; }
+  constexpr PhaseIndex phase_index() const noexcept
+  {
+    return m_lp_key_.phase_index;
+  }
 
   [[nodiscard]]
-  constexpr std::span<const DependentVariable> dependent_variables() const noexcept
+  constexpr std::span<const DependentVariable> dependent_variables()
+      const noexcept
   {
     return m_dependent_variables_;
   }
 
-  constexpr auto add_dependent_variable(SceneIndex scene_index,
-                                       PhaseIndex phase_index,
-                                       Index col) noexcept -> DependentVariable&
+  constexpr auto add_dependent_variable(const LPKey& lp_key, Index col) noexcept
+      -> const DependentVariable&
   {
-    return m_dependent_variables_.emplace_back(
-      LPKey{.scene_index = scene_index, .phase_index = phase_index}, col);
+    return m_dependent_variables_.emplace_back(lp_key, col);
   }
 
   template<typename ScenarioLP, typename StageLP>
-  constexpr auto add_dependent_variable(const ScenarioLP& scenario,
-                                      const StageLP& stage,
-                                      Index col) noexcept -> DependentVariable&
+  constexpr auto&& add_dependent_variable(const ScenarioLP& scenario,
+                                          const StageLP& stage,
+                                          Index col) noexcept
   {
-    return add_dependent_variable(scenario.scene_index(), stage.phase_index(), col);
+    return add_dependent_variable(LPKey {.scene_index = scenario.scene_index(),
+                                         .phase_index = stage.phase_index()},
+                                  col);
   }
 
 private:
   LPKey m_lp_key_;
-  Index m_col_{unknown_index};
+  Index m_col_ {unknown_index};
   std::vector<DependentVariable> m_dependent_variables_;
 };
 
-} // namespace gtopt
+}  // namespace gtopt
--- test/source/test_state_variable.cpp
+++ test/source/test_state_variable.cpp
@@ -1,6 +1,6 @@
 #include <doctest/doctest.h>
-#include <gtopt/state_variable.hpp>
 #include <gtopt/linear_problem.hpp>
+#include <gtopt/state_variable.hpp>
 
 using namespace gtopt;
 
@@ -8,10 +8,10 @@ TEST_CASE("StateVariable key method")
 {
   SUBCASE("Basic key formation")
   {
-    auto key = StateVariable::key("TestClass", 123, "col_name", 
-                                PhaseIndex{1}, StageUid{2});
-    CHECK(key.scenario_uid == ScenarioUid{unknown_uid});
-    CHECK(key.stage_uid == StageUid{2});
+    auto key = StateVariable::key(
+        "TestClass", 123, "col_name", PhaseIndex {1}, StageUid {2});
+    CHECK(key.scenario_uid == ScenarioUid {unknown_uid});
+    CHECK(key.stage_uid == StageUid {2});
     CHECK(key.class_name == "TestClass");
     CHECK(key.uid == 123);
     CHECK(key.col_name == "col_name");
@@ -19,24 +19,32 @@ TEST_CASE("StateVariable key method")
 
   SUBCASE("Key formation with all parameters")
   {
-    auto key = StateVariable::key("TestClass", 123, "col_name",
-                                PhaseIndex{1}, StageUid{2},
-                                SceneIndex{3}, ScenarioUid{4});
-    CHECK(key.scenario_uid == ScenarioUid{4});
-    CHECK(key.stage_uid == StageUid{2});
+    auto key = StateVariable::key("TestClass",
+                                  123,
+                                  "col_name",
+                                  PhaseIndex {1},
+                                  StageUid {2},
+                                  SceneIndex {3},
+                                  ScenarioUid {4});
+    CHECK(key.scenario_uid == ScenarioUid {4});
+    CHECK(key.stage_uid == StageUid {2});
     CHECK(key.class_name == "TestClass");
     CHECK(key.uid == 123);
     CHECK(key.col_name == "col_name");
-    CHECK(key.lp_key.scene_index == SceneIndex{3});
-    CHECK(key.lp_key.phase_index == PhaseIndex{1});
+    CHECK(key.lp_key.scene_index == SceneIndex {3});
+    CHECK(key.lp_key.phase_index == PhaseIndex {1});
   }
 
   SUBCASE("Key comparison")
   {
-    auto key1 = StateVariable::key("Class1", 1, "col1", PhaseIndex{1}, StageUid{1});
-    auto key2 = StateVariable::key("Class1", 1, "col1", PhaseIndex{1}, StageUid{1});
-    auto key3 = StateVariable::key("Class2", 1, "col1", PhaseIndex{1}, StageUid{1});
-    auto key4 = StateVariable::key("Class1", 2, "col1", PhaseIndex{1}, StageUid{1});
+    auto key1 =
+        StateVariable::key("Class1", 1, "col1", PhaseIndex {1}, StageUid {1});
+    auto key2 =
+        StateVariable::key("Class1", 1, "col1", PhaseIndex {1}, StageUid {1});
+    auto key3 =
+        StateVariable::key("Class2", 1, "col1", PhaseIndex {1}, StageUid {1});
+    auto key4 =
+        StateVariable::key("Class1", 2, "col1", PhaseIndex {1}, StageUid {1});
 
     CHECK(key1 == key2);
     CHECK(key1 != key3);
@@ -48,49 +56,52 @@ TEST_CASE("StateVariable construction and basic properties")
 {
   SUBCASE("Basic construction")
   {
-    StateVariable var({SceneIndex{1}, PhaseIndex{2}}, 3);
-    
+    StateVariable var(
+        {.scene_index = SceneIndex {1}, .phase_index = PhaseIndex {2}}, 3);
+
     CHECK(var.col() == 3);
-    CHECK(var.scene_index() == SceneIndex{1});
-    CHECK(var.phase_index() == PhaseIndex{2});
+    CHECK(var.scene_index() == SceneIndex {1});
+    CHECK(var.phase_index() == PhaseIndex {2});
     CHECK(var.dependent_variables().empty());
   }
 
   SUBCASE("With dependent variables")
   {
-    StateVariable var({SceneIndex{1}, PhaseIndex{2}}, 3);
+    StateVariable var(
+        {.scene_index = SceneIndex {1}, .phase_index = PhaseIndex {2}}, 3);
 
-    var.add_dependent_variable(SceneIndex{4}, PhaseIndex{5}, 10);
-    var.add_dependent_variable(SceneIndex{6}, PhaseIndex{7}, 20);
+    var.add_dependent_variable(SceneIndex {4}, PhaseIndex {5}, 10);
+    var.add_dependent_variable(SceneIndex {6}, PhaseIndex {7}, 20);
 
     const auto deps = var.dependent_variables();
     REQUIRE(deps.size() == 2);
-    CHECK(deps[0].lp_key.scene_index == SceneIndex{4});
-    CHECK(deps[0].lp_key.phase_index == PhaseIndex{5});
+    CHECK(deps[0].lp_key.scene_index == SceneIndex {4});
+    CHECK(deps[0].lp_key.phase_index == PhaseIndex {5});
     CHECK(deps[0].col == 10);
-    CHECK(deps[1].lp_key.scene_index == SceneIndex{6});
-    CHECK(deps[1].lp_key.phase_index == PhaseIndex{7});
+    CHECK(deps[1].lp_key.scene_index == SceneIndex {6});
+    CHECK(deps[1].lp_key.phase_index == PhaseIndex {7});
     CHECK(deps[1].col == 20);
   }
 }
 
 TEST_CASE("StateVariable dependent variables")
 {
-  StateVariable var({SceneIndex{1}, PhaseIndex{2}}, 3);
+  StateVariable var(
+      {.scene_index = SceneIndex {1}, .phase_index = PhaseIndex {2}}, 3);
 
   SUBCASE("Adding single dependent variable")
   {
-    auto& dep = var.add_dependent_variable(SceneIndex{4}, PhaseIndex{5}, 6);
-    CHECK(dep.lp_key.scene_index == SceneIndex{4});
-    CHECK(dep.lp_key.phase_index == PhaseIndex{5});
+    auto& dep = var.add_dependent_variable(SceneIndex {4}, PhaseIndex {5}, 6);
+    CHECK(dep.lp_key.scene_index == SceneIndex {4});
+    CHECK(dep.lp_key.phase_index == PhaseIndex {5});
     CHECK(dep.col == 6);
   }
 
   SUBCASE("Adding multiple dependent variables")
   {
-    var.add_dependent_variable(SceneIndex{4}, PhaseIndex{5}, 6);
-    var.add_dependent_variable(SceneIndex{7}, PhaseIndex{8}, 9);
-    
+    var.add_dependent_variable(SceneIndex {4}, PhaseIndex {5}, 6);
+    var.add_dependent_variable(SceneIndex {7}, PhaseIndex {8}, 9);
+
     const auto deps = var.dependent_variables();
     REQUIRE(deps.size() == 2);
     CHECK(deps[0].col == 6);
