9fc1e7f3602ad229e05faa06da214e3244114a25
Marcelo Matus <mmatus@blackbeareng.cl>
2025-06-22 12:43:40 -0400

    refactor: group scene and phase indices into LPKey struct

--- include/gtopt/state_variable.hpp
+++ include/gtopt/state_variable.hpp
@@ -27,11 +27,22 @@ namespace gtopt
 class StateVariable
 {
 public:
+  struct LPKey
+  {
+    SceneIndex scene_index {unknown_index};
+    PhaseIndex phase_index {unknown_index};
+
+    auto operator<=>(const LPKey&) const = default;
+  };
+
   struct Key
   {
-    ScenarioUid scenario_uid;
-    StageUid stage_uid;
-    Uid uid;
+    LPKey lp_key;
+
+    ScenarioUid scenario_uid {unknown_uid};
+    StageUid stage_uid {unknown_uid};
+
+    Uid uid {unknown_uid};
     std::string_view class_name;
     std::string_view col_name;
 
@@ -39,55 +50,75 @@ public:
   };
 
   [[nodiscard]]
-  static auto key(std::string_view class_name,
-                  Uid uid,
-                  std::string_view col_name,
-                  StageUid stage_uid = StageUid {unknown_uid},
-                  ScenarioUid scenario_uid = ScenarioUid {unknown_uid}) -> Key
+  static auto key(const std::string_view class_name,
+                  const Uid uid,
+                  const std::string_view col_name,
+                  const PhaseIndex phase_index,
+                  const StageUid stage_uid,
+                  const SceneIndex scene_index = SceneIndex {unknown_index},
+                  const ScenarioUid scenario_uid = ScenarioUid {unknown_uid})
+      -> Key
   {
-    return {.scenario_uid = scenario_uid,
+    return {.lp_key = {.scene_index = scene_index, .phase_index = phase_index},
+            .scenario_uid = scenario_uid,
             .stage_uid = stage_uid,
             .uid = uid,
             .class_name = class_name,
             .col_name = col_name};
   }
 
-  template<typename Element>
+  template<typename ScenarioLP, typename StageLP>
   [[nodiscard]]
-  static auto key(const Element& element,
-                  std::string_view col_name,
-                  StageUid stage_uid = StageUid {unknown_uid},
-                  ScenarioUid scenario_uid = ScenarioUid {unknown_uid}) -> Key
+  static auto key(const ScenarioLP& scenario,
+                  const StageLP& stage,
+                  const std::string_view class_name,
+                  const Uid element_uid,
+                  const std::string_view col_name) -> Key
   {
-    return key(
-        element.class_name(), element.uid(), col_name, stage_uid, scenario_uid);
+    return key(class_name,
+               element_uid,
+               col_name,
+               stage.phase_index(),
+               stage.uid(),
+               scenario.scene_index(),
+               scenario.uid());
   }
 
-  constexpr explicit StateVariable(SceneIndex scene_index,
-                                   PhaseIndex phase_index,
-                                   Index col) noexcept
-      : m_scene_index_(scene_index)
-      , m_phase_index_(phase_index)
+  template<typename StageLP>
+  [[nodiscard]]
+  static auto key(const StageLP& stage,
+                  const std::string_view class_name,
+                  const Uid element_uid,
+                  const std::string_view col_name) -> Key
+  {
+    return key(
+        class_name, element_uid, col_name, stage.phase_index(), stage.uid());
+  }
+
+  constexpr explicit StateVariable(const LPKey& lp_key, Index col) noexcept
+      : m_lp_key_(lp_key)
       , m_col_(col)
   {
   }
 
-  [[nodiscard]] constexpr Index col() const noexcept { return m_col_; }
+  [[nodiscard]]
+  constexpr Index col() const noexcept
+  {
+    return m_col_;
+  }
 
   struct DependentVariable
   {
-    SceneIndex scene_index {unknown_index};
-    PhaseIndex phase_index {unknown_index};
+    LPKey lp_key;
     Index col {unknown_index};
   };
-
-  using dependent_variable_t = DependentVariable;
 
   constexpr auto&& add_dependent_variable(SceneIndex scene_index,
                                           PhaseIndex phase_index,
                                           Index col) noexcept
   {
-    return m_dependent_variables_.emplace_back(scene_index, phase_index, col);
+    return m_dependent_variables_.emplace_back(
+        LPKey {.scene_index = scene_index, .phase_index = phase_index}, col);
   }
 
   template<typename ScenarioLP, typename StageLP>
@@ -105,22 +136,17 @@ public:
     return m_dependent_variables_;
   }
 
-  [[nodiscard]] constexpr SceneIndex scene_index() const noexcept
+  [[nodiscard]]
+  constexpr const auto& lp_key() const noexcept
   {
-    return m_scene_index_;
-  }
-
-  [[nodiscard]] constexpr PhaseIndex phase_index() const noexcept
-  {
-    return m_phase_index_;
+    return m_lp_key_;
   }
 
 private:
-  SceneIndex m_scene_index_ {unknown_index};
-  PhaseIndex m_phase_index_ {unknown_index};
+  LPKey m_lp_key_;
   Index m_col_ {unknown_index};
 
-  std::vector<dependent_variable_t> m_dependent_variables_;
+  std::vector<DependentVariable> m_dependent_variables_;
 };
 
 }  // namespace gtopt
