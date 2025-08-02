bb9637ea2d79d7a56eae683618ded3fb0407e1f1
Marcelo Matus <mmatus@blackbeareng.cl>
2025-08-02 02:05:08 -0400

    refactor: remove structured binding support and clean up generator profile

--- include/gtopt/generator_profile.hpp
+++ include/gtopt/generator_profile.hpp
@@ -24,17 +24,6 @@ struct GeneratorProfile
   SingleId generator {unknown_uid};
   STBRealFieldSched profile {};
   OptTRealFieldSched scost {};
-
-  // Structured binding support
-  template<std::size_t I>
-  [[nodiscard]] constexpr auto get() const noexcept {
-    if constexpr (I == 0) { return uid; }
-    else if constexpr (I == 1) { return name; }
-    else if constexpr (I == 2) { return active; }
-    else if constexpr (I == 3) { return generator; }
-    else if constexpr (I == 4) { return profile; }
-    else if constexpr (I == 5) { return scost; }
-  }
 };
 
 }  // namespace gtopt
