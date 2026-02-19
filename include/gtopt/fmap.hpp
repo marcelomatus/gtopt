9ea20f4c2846e5dc7b6c0d3024e6637019d447e4
Marcelo Matus <you@example.com>
2026-02-18 21:29:06 -0300

    (no commit message provided)

--- include/gtopt/fmap.hpp
+++ include/gtopt/fmap.hpp
@@ -62,6 +62,14 @@ void map_reserve(Map& map, Size n)
 }  // namespace gtopt
 
 #  else
+namespace gtopt
+{
+template<typename Map, typename Size>
+void map_reserve(Map& map, Size n)
+{
+  map.reserve(n);
+}
+}  // namespace gtopt
 
 #    include <flat_map>
 namespace gtopt
@@ -70,17 +78,30 @@ namespace gtopt
 template<typename key_type, typename value_type>
 using flat_map = std::flat_map<key_type, value_type>;
 
-template<class Map, typename Size>
-void map_reserve(Map& map, Size new_cap)
-{
-  auto containers = std::move(map).extract();
-  containers.keys.reserve(static_cast<size_t>(new_cap));
-  containers.values.reserve(static_cast<size_t>(new_cap));
-  map.replace(std::move(containers));
-}
-
 }  // namespace gtopt
 
 #  endif
 
 #endif
+
+#if __has_include(<flat_map>)
+
+#  include <flat_map>
+
+namespace gtopt
+{
+
+template<typename key_type, typename value_type, typename Size>
+void map_reserve(std::flat_map<key_type, value_type>& map, Size new_cap)
+{
+  auto containers = std::move(map).extract();
+  containers.keys.reserve(static_cast<size_t>(new_cap));
+  containers.values.reserve(static_cast<size_t>(new_cap));
+
+  map.replace(std::move(containers.keys),
+              std::move(containers.values));  // NOLINT
+}
+
+}  // namespace gtopt
+
+#endif  //
