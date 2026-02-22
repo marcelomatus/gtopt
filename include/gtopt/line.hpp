520490f40e35747e09790570f433d56c54985d7a
Marcelo Matus <you@example.com>
2026-02-22 05:16:03 -0300

    add units to the line declaration

--- include/gtopt/line.hpp
+++ include/gtopt/line.hpp
@@ -29,20 +29,20 @@ struct Line
   SingleId bus_a {unknown_uid};  ///< From-bus ID
   SingleId bus_b {unknown_uid};  ///< To-bus ID
 
-  OptTRealFieldSched voltage {};  ///< Line voltage level
-  OptTRealFieldSched resistance {};  ///< Line resistance
-  OptTRealFieldSched reactance {};  ///< Line reactance
-  OptTRealFieldSched lossfactor {};  ///< Line loss factor
-  OptTBRealFieldSched tmax_ba {};  ///< Minimum power flow
-  OptTBRealFieldSched tmax_ab {};  ///< Maximum power flow
-  OptTRealFieldSched tcost {};  ///< Transmission cost
+  OptTRealFieldSched voltage {};  ///< Line voltage level [KW]
+  OptTRealFieldSched resistance {};  ///< Line resistance [Ohm]
+  OptTRealFieldSched reactance {};  ///< Line reactance  [Ohm]
+  OptTRealFieldSched lossfactor {};  ///< Line loss factor [p.u.]
+  OptTBRealFieldSched tmax_ba {};  ///< Minimum power flow [MW]
+  OptTBRealFieldSched tmax_ab {};  ///< Maximum power flow [MW]
+  OptTRealFieldSched tcost {};  ///< Transmission cost [$/MWh]
 
-  OptTRealFieldSched capacity {};  ///< Installed capacity
-  OptTRealFieldSched expcap {};  ///< Expansion capacity
-  OptTRealFieldSched expmod {};  ///< Expansion module
-  OptTRealFieldSched capmax {};  ///< Maximum capacity
-  OptTRealFieldSched annual_capcost {};  ///< Capacity cost
-  OptTRealFieldSched annual_derating {};  /// Derating factor
+  OptTRealFieldSched capacity {};  ///< Installed capacity [MW]
+  OptTRealFieldSched expcap {};  ///< Expansion capacity [MW]
+  OptTRealFieldSched expmod {};  ///< Expansion module [MW]
+  OptTRealFieldSched capmax {};  ///< Maximum capacity [MW]
+  OptTRealFieldSched annual_capcost {};  ///< Capacity cost [$/MW-year]
+  OptTRealFieldSched annual_derating {};  /// Derating factor [p.u./year]
 };
 
 }  // namespace gtopt
